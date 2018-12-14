d9381332c1a6db7549baed2eccf82debeddfeba1
Annar Gaustad <Annar.Gaustad@nov.com>
2018-12-14 21:04:03 +0100

    Measurement Class
    Related Work Items: #39

---
+++ FresnoSolution/LanterneRouge.Fresno.Data/DataAccess/Entities/Measurement.cs
@@ -0,0 +1,15 @@
+﻿namespace LanterneRouge.Fresno.DataLayer.DataAccess.Entities
+{
+    public class Measurement
+    {
+        public int Id { get; set; }
+
+        public byte HeartRate { get; set; }
+
+        public float Lactate { get; set; }
+
+        public float Load { get; set; }
+
+        public int StepTestId { get; set; }
+    }
+}
