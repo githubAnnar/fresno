94161ad2717eaca88591bf531237087104274ac1
Annar Gaustad <Annar.Gaustad@nov.com>
2018-12-14 21:07:01 +0100

    StepTestClass
    Related Work Items: #40

---
+++ FresnoSolution/LanterneRouge.Fresno.Data/DataAccess/Entities/StepTest.cs
@@ -0,0 +1,11 @@
+﻿namespace LanterneRouge.Fresno.DataLayer.DataAccess.Entities
+{
+    public class StepTest
+    {
+        public int Id { get; set; }
+
+        public int Sequence { get; set; }
+
+        public int UserId { get; set; }
+    }
+}
