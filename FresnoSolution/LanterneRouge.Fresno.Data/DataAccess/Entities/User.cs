bc62281dc4d346f36c9a306a101d5730999a5ee7
Annar Gaustad <Annar.Gaustad@nov.com>
2018-12-14 21:11:17 +0100

    User Class
    Related Work Items: #41

---
+++ FresnoSolution/LanterneRouge.Fresno.Data/DataAccess/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿namespace LanterneRouge.Fresno.DataLayer.DataAccess.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string  FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+    }
+}
