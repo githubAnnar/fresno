eecea8270b9dcd84ed7804c500404b3431f7b862
Annar Gaustad <annar.gaustad@nov.com>
2019-02-18 23:23:20 +0100

    Added LT Calculation class

--- FresnoSolution/LanterneRouge.Fresno.Calculations/FblcCalculation.cs
+++ FresnoSolution/LanterneRouge.Fresno.Calculations/FblcCalculation.cs
@@ -52,43 +52,6 @@ namespace LanterneRouge.Fresno.Calculations
             }
         }
 
-        #endregion
-
-        //public float GetLt()
-        //{
-        //    var delta = new List<Tuple<double, double>>();
-        //    for (var k = Loads.Min() + 1d; k < Loads.Max() - 1d; k = k + 0.93)
-        //    {
-        //        var p = Fit.LinearCombinationFunc(
-        //            Loads.ToArray(),
-        //            Lactates.ToArray(),
-        //            x => 1d,
-        //            x => x >= k ? x : k,
-        //            x => x < k ? 0 : (x - k));
-
-        //        var pVal = Loads.Select(l => p(l)).ToList();
-        //        //var results = (from load in Loads from lactate in Lactates from calc in pVal select new Result { x = load, y = lactate, Y = calc }).ToList();
-        //        //var results = (Loads.Zip(Lactates, (first, second) => new { x = first, y = second }).Zip(pVal, (first, second) => new { x = first.x, y = first.y, Y = second })).ToList();
-
-        //        //var seg1ObsYAve = results.Where(r => r.x < k).Average(r => r.Y);
-        //        //var seg2ObsYAve = results.Where(r => r.x > k).Average(r => r.Y);
-
-        //        //var teller1 = results.Where(r => r.x < k).Sum(r => Math.Pow(r.y - r.Y, 2d));
-        //        //var teller2 = results.Where(r => r.x >= k).Sum(r => Math.Pow(r.y - r.Y, 2d));
-        //        //var nevner1 = results.Where(r => r.x < k).Sum(r => Math.Pow(r.y - seg1ObsYAve, 2d));
-        //        //var nevner2 = results.Where(r => r.x >= k).Sum(r => Math.Pow(r.y - seg2ObsYAve, 2d));
-
-        //        //var r1 = 1 - (teller1 / nevner1);
-        //        //var r2 = 1 - (teller2 / nevner2);
-        //        var cod = GoodnessOfFit.CoefficientOfDetermination(pVal, Lactates);
-
-
-        //        delta.Add(new Tuple<double, double>(k, cod));
-        //    }
-
-
-
-        //    return (float)0;
-        //}        
+        #endregion      
     }
 }
---
+++ FresnoSolution/LanterneRouge.Fresno.Calculations/LTCalculation.cs
@@ -0,0 +1,98 @@
+﻿using LanterneRouge.Fresno.Calculations.Base;
+using LanterneRouge.Fresno.DataLayer.DataAccess.Entities;
+using MathNet.Numerics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LanterneRouge.Fresno.Calculations
+{
+    public class LTCalculation : BaseCalculation
+    {
+        #region Fields
+
+        private float _lactateThreshold = 0;
+        private float _heartRateThreshold = 0;
+
+        #endregion
+
+        #region Constructors
+
+        public LTCalculation(IEnumerable<Measurement> measurements) : base(measurements)
+        { }
+
+        #endregion
+
+        #region Properties
+
+        public override float LoadThreshold
+        {
+            get
+            {
+                if (Measurements != null)
+                {
+                    var baseMeasurements = Measurements.ToList();
+                    if (_lactateThreshold == 0 && baseMeasurements.Count > 2)
+                    {
+                        var baseLine = baseMeasurements[0];
+                        _lactateThreshold = (float)FindLoadFromLactate(baseMeasurements[0].Lactate);
+                    }
+                }
+
+                return _lactateThreshold;
+            }
+        }
+
+        public override float HeartRateThreshold
+        {
+            get
+            {
+                if (_heartRateThreshold == 0)
+                {
+                    _heartRateThreshold = (float)FittedHeartRateCurve(LoadThreshold);
+                }
+
+                return _heartRateThreshold;
+            }
+        }
+
+        #endregion
+
+        public float GetLt()
+        {
+            var delta = new List<Tuple<double, double>>();
+            for (var k = Loads.Min() + 1d; k < Loads.Max() - 1d; k = k + 0.93)
+            {
+                var p = Fit.LinearCombinationFunc(
+                    Loads.ToArray(),
+                    Lactates.ToArray(),
+                    x => 1d,
+                    x => x >= k ? x : k,
+                    x => x < k ? 0 : (x - k));
+
+                var pVal = Loads.Select(l => p(l)).ToList();
+                //var results = (from load in Loads from lactate in Lactates from calc in pVal select new Result { x = load, y = lactate, Y = calc }).ToList();
+                //var results = (Loads.Zip(Lactates, (first, second) => new { x = first, y = second }).Zip(pVal, (first, second) => new { x = first.x, y = first.y, Y = second })).ToList();
+
+                //var seg1ObsYAve = results.Where(r => r.x < k).Average(r => r.Y);
+                //var seg2ObsYAve = results.Where(r => r.x > k).Average(r => r.Y);
+
+                //var teller1 = results.Where(r => r.x < k).Sum(r => Math.Pow(r.y - r.Y, 2d));
+                //var teller2 = results.Where(r => r.x >= k).Sum(r => Math.Pow(r.y - r.Y, 2d));
+                //var nevner1 = results.Where(r => r.x < k).Sum(r => Math.Pow(r.y - seg1ObsYAve, 2d));
+                //var nevner2 = results.Where(r => r.x >= k).Sum(r => Math.Pow(r.y - seg2ObsYAve, 2d));
+
+                //var r1 = 1 - (teller1 / nevner1);
+                //var r2 = 1 - (teller2 / nevner2);
+                var cod = GoodnessOfFit.CoefficientOfDetermination(pVal, Lactates);
+
+
+                delta.Add(new Tuple<double, double>(k, cod));
+            }
+
+
+
+            return (float)0;
+        }
+    }
+}
