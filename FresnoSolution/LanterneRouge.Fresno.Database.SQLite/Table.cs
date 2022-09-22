0dc35332c0f11e086f9b8d58f6ba037d81aa32da
Annar Gaustad <annar.gaustad@gmail.com>
2022-09-22 23:12:09 +0200

    Added table class

---
+++ FresnoSolution/LanterneRouge.Fresno.Database.SQLite/Table.cs
@@ -0,0 +1,16 @@
+﻿namespace LanterneRouge.Fresno.Database.SQLite
+{
+    public class Table
+    {
+        public Table(string name)
+        {
+            Name = name;
+        }
+
+        #region Properties
+
+        public string Name { get; set; }
+
+        #endregion
+    }
+}
