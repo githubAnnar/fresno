bfa5016d94bbb6abe6c4c057043ee8c4c3018cb5
Annar Gaustad <annar.gaustad@gmail.com>
2022-10-01 15:12:56 +0200

    Added DMax calculation class

---
+++ FresnoSolution/LanterneRouge.Fresno.Calculations/DmaxCalculation.cs
@@ -0,0 +1,40 @@
+﻿using LanterneRouge.Fresno.Calculations.Base;
+using LanterneRouge.Fresno.DataLayer.DataAccess.Entities;
+using System.Collections.Generic;
+
+namespace LanterneRouge.Fresno.Calculations
+{
+    public class DmaxCalculation : BaseCalculation
+    {
+        #region Fields
+
+        private float _lactateThreshold = 0;
+        private float _heartRateThreshold = 0;
+
+        #endregion
+
+        #region Constructors
+
+        public DmaxCalculation(IEnumerable<Measurement> measurements) : base(measurements)
+        { }
+
+        #endregion
+
+        #region Properties
+
+        public override float LoadThreshold => base.LoadThreshold;
+
+        public override float HeartRateThreshold => base.HeartRateThreshold;
+
+        #endregion
+
+        #region Methods
+
+        private double CalculateDmax()
+        {
+            return 0d;
+        }
+
+        #endregion
+    }
+}
