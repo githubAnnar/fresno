88ec5987eb22db765cc066a2ed42838df024cfff
Annar Gaustad <annar.gaustad@gmail.com>
2023-01-09 21:34:33 +0100

    ZoneSetting ViewModel

---
+++ FresnoSolution/LanterneRouge.Fresno.WpfClient/UserControls/ViewModel/ZoneSettingViewModel.cs
@@ -0,0 +1,18 @@
+﻿using LanterneRouge.Fresno.WpfClient.MVVM;
+
+namespace LanterneRouge.Fresno.WpfClient.UserControls.ViewModel
+{
+    public class ZoneSettingViewModel : ViewModelBase
+    {
+        private double _zoneValue = 0d;
+        private string _zoneName = "Zone";
+
+        public double ZoneValue { get => _zoneValue; set { _zoneValue = value; OnPropertyChanged(); } }
+
+        public string ZoneName { get => _zoneName; set { _zoneName = value; OnPropertyChanged(); } }
+
+        public int Maximum { get; set; } = 20;
+
+        public double TickFrequency { get; set; } = 1d;
+    }
+}
